66a5c35a41a6a274fa0863708e7196b005a2e3b6
JustinMartin7x <justin.martin7x@gmail.com>
2020-12-01 13:18:27 -0800

    wrote test for findname function

---
+++ tests.js
@@ -0,0 +1,12 @@
+
+const spot = { name: 'spot', age: 5, weight: '20 lbs' }
+
+
+describe('findName', () => {
+    it('should take in an object and return the name property', () => {
+        const name = findName(spot);
+
+        expect(name)
+            .toEqual('spot')
+    });
+});
