ff558970fb6b4319839228ed0c5f938396004b94
JustinMartin7x <justin.martin7x@gmail.com>
2020-12-01 16:13:00 -0800

    just commiting to make sure its all here

--- fetchQuotes.js
+++ fetchQuotes.js
@@ -3,10 +3,8 @@ const request = require('superagent')
 const fetchQuotes = async () => {
     const { body } = await request
         .get('futuramaapi.herokuapp.com/api/quotes?')
-
     const { character: name, quote: text, image } = body[0]
     return { name, text, image }
-
 }
 module.exports = {
     fetchQuotes
